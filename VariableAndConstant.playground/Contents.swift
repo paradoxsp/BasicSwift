45c674b13c0ad37e1e5df20385bcdef6ee31e9c5
paradoxSP <paradoxjava@gmail.com>
2017-12-09 11:01:13 +0700

    test

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+//Library
+import UIKit
+
+// Variable
+
+var nameString = "Paradox SP"
+var numInt = 123
+var numDouble = 123.50
+var statusBol = true
+
+// Change value
+
+nameString = "SP"
+numInt = 321
+numDouble = 321.25
+statusBol = false
+
+
+
+
+
+

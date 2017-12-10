313c61584fd60e6a6cb368ae1cbaffd006a995aa
paradoxSP <paradoxjava@gmail.com>
2017-12-10 14:06:24 +0700

    Function OK

--- ClassAndFunction.playground/Contents.swift
+++ ClassAndFunction.playground/Contents.swift
@@ -8,7 +8,30 @@ class MyClass{
     
     //Explicit
     var numberInt = 123
-    var nameString = "Paradox" 
+    var nameString = "Paradox"
+    
+    
+    // Create function void type
+    func myFunctionTypeVoid() -> Void {
+        print("This is void function")
+    }
+    
+    func myFunctionTypeVoidHaveArgument(surname: String) -> Void {
+        
+        let surnameString = surname
+        
+        print("Surname >> \(surnameString)")
+    }
+    
+    // Create function return type
+    func calculateArea(base: Double, height: Double) -> Double {
+        // var areaDouble = 0.5 * base * height
+        return 0.5 * base * height
+
+    }
+    
+    
+    
 }
 
 // Call resource of class
@@ -26,3 +49,16 @@ myClass.nameString = "SP"
 
 print("After number >> \(myClass.numberInt)")
 print("After string >> \(myClass.nameString)")
+
+// Call function
+
+myClass.myFunctionTypeVoid()
+
+myClass.myFunctionTypeVoidHaveArgument(surname: "Prommala")
+
+var answerDouble = myClass.calculateArea(base: 10, height: 25)
+
+
+
+
+

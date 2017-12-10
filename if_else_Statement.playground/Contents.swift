40892be31aedb5f3e388cf803ce7744b11ac638f
paradoxSP <paradoxjava@gmail.com>
2017-12-10 10:27:52 +0700

    test if else statement

---
+++ if_else_Statement.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let ageInt = 15
+
+if (ageInt < 18) {
+    print("You are young")
+}else{
+    print("You are old")
+}
+
+
+
+let nameString = "Paradox"
+let friendStrings = ["Paradox", "Java"]
+
+if (nameString == friendStrings[0]) {
+    print("My friend is \(nameString)")
+}else{
+    print("Not my friend")
+}

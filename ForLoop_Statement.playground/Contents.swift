4e46e54bc6e174c49b02e2ec818e07a74cdb6400
paradoxSP <paradoxjava@gmail.com>
2017-12-10 10:49:20 +0700

    For loop sample

---
+++ ForLoop_Statement.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let constantInts = [11,22,33,44,55]
+var timesInt = 0
+
+for myConstantInts in constantInts {
+    
+    timesInt += 1
+    
+    print("timeInts >> \(timesInt) value of myConstantInts >> \(myConstantInts)")
+}
